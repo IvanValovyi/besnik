6009699df2948739bc123467941a1f974e7fb32c
TimaVol <tima.voloshuk@gmail.com>
2023-02-11 14:04:48 +0200

    features wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Fire } from "@/common/icons";
+import Features from "@/components/Features";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Features />
+    </>
+  );
 }
