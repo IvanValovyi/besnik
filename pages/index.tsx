67ca2fec8bfb0cbfe48f473135b93f2273406f48
IvanValovyi <pan.ivan.17.0@gmail.com>
2023-02-12 21:05:30 +0200

    Added Handoff block, some other changes.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Customers from "@/components/Customers";
 import Header from "@/components/Header";
 import AboutUs from "@/components/AboutUs";
 import Subscribe from "@/components/Subscribe";
+import Footer from "@/components/Footer";
+import Handoff from "@/components/Handoff";
 
 export default function Home() {
   return (
@@ -13,7 +15,9 @@ export default function Home() {
       <Features />
       <AboutUs />
       <Customers />
-      <Subscribe />
+      <Handoff />
+      {/* <Subscribe /> */}
+      <Footer />
     </>
   );
 }
