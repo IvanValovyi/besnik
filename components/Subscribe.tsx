b06cbc465401d29f1911bf8fc66827bca8353e6a
TimaVol <tima.voloshuk@gmail.com>
2023-02-16 17:34:25 +0200

    Subscribe done

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,22 +1,21 @@
 import CircleText from "@/public/icons/circleText.svg";
-import CardPic from "@/public/images/card.png";
 import Image from "next/image";
 
 export default function Subscribe() {
   return (
     <div className="bg-blueDark mt-[120px]">
-      <div className="container py-[115px] flex">
-        <div className="flex pt-[70px]">
-          <div className="pt-[20px]">
-            <p className="w-[120px] mb-[28px] text-grayDark text-1 leading-[27px] font-bold tracking-wide">
+      <div className="container lg:py-[115px] flex justify-between flex-col lg:flex-row">
+        <div className="flex justify-between flex-col sm:flex-row w-full sm:pt-[70px]">
+          <div className="pt-[36px] flex items-end justify-between sm:block sm:w-1/2 lg:w-auto">
+            <p className="sm:w-[120px] sm:mb-[28px] text-grayDark text-1 leading-[27px] font-bold tracking-wide">
               Subscribe to get updated news
             </p>
-            <CircleText />
+            <CircleText className="animate-spin-slow" />
           </div>
-          <div className="w-[8px] h-[133px] mx-[80px] bg-grayLight" />
+          <div className="w-full h-[3px] my-[20px] sm:w-[8px] sm:h-[133px] sm:mx-[80px] bg-grayLight" />
 
-          <div className="pt-[11px]">
-            <h1 className="mb-[42px] family-bai_jamjuree">
+          <div className="sm:w-2/3 lg:w-auto">
+            <h1 className="text-1 w-full lg:w-[406px] xl:w-full mb-[42px] family-bai_jamjuree">
               Subscribe our newsletter
             </h1>
 
@@ -26,7 +25,14 @@ export default function Subscribe() {
           </div>
         </div>
 
-        <Image src={CardPic} alt="cart" />
+        <div className="relative mx-auto lg:mx-0 h-[360px] sm:h-[500px] lg:h-auto max-w-[529px] w-full">
+          <Image
+            src="/images/card.png"
+            fill
+            alt="cart"
+            sizes="(max-width: 768px) 100vw"
+          />
+        </div>
       </div>
     </div>
   );
